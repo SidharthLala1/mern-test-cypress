f0bfd9f6fe78e44354253f84e3c2c483c2544198
Sidharth Lala <sidharth@ziplyne.com>
2020-03-02 00:26:12 +0530

    Updated the code to support fixture

--- cypress/integration/todo-spec.js
+++ cypress/integration/todo-spec.js
@@ -1,32 +1,35 @@
+/**
+ * API Test case for create TODO-01
+ */
+
 describe('CreateTodo', () => {
-    let data = {
-        "todo_description": "Test1",
-        "todo_responsible": "Test1",
-        "todo_priority": "High",
-        "todo_completed": false
-    }
+  
     it('Create a todo with description and responsible', () => {
-        cy.fixture('todos').as('todoslist')
         cy.server()
+        cy.fixture('todos.json').as('todoslist')
         cy.route({
             method: 'POST',
             url: '/todos/add',
             response: '{"result": "todo added successfully"}',
             status: 200
-        });
+        }).as('todoslist');
+        
     });
 
 });
 
+/**
+ * API Test case for Lisiting all the TODOS-02
+ */
 
 describe('Listtodo', function() {
     it('feth the list of todos', function() {
-        cy.fixture('todos').as('todoslist')
         cy.server().should((server) => {
             expect(server.delay).to.eq(0)
             expect(server.method).to.eq('GET')
             expect(server.status).to.eq(200)
         })
+        cy.fixture('todos.json').as('todoslist')
         cy.route({
             method: 'GET',
             url: '/todos/',
@@ -37,11 +40,14 @@ describe('Listtodo', function() {
 
 });
 
+/**
+ * API Test case for deleting a the TODO-03
+ */
 
 describe('Deletetodo', function () {
     it('Delete the todos', function () {
-        cy.fixture('todos').as('todoslist')
         cy.server()
+        cy.fixture('todos.json').as('todoslist')
         cy.route({
             method: 'DELETE',
             url: '/todos/delete/5e56b883eba37d3ce4d6297a',
@@ -52,10 +58,14 @@ describe('Deletetodo', function () {
 
 });
 
+/**
+ * API Test case for updating a the TODO-04
+ */
+
 describe('Updatetodo', function () {
     it('Update a todos', function () {
-        cy.fixture('todos').as('todoslist')
         cy.server()
+        cy.fixture('todos.json').as('todoslist')
         cy.route({
             method: 'PUT',
             url: '/todos/update/5e56b72eeba37d3ce4d62979',
